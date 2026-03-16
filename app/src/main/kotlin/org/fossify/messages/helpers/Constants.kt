6e82221bd1025b17c9f372bc8c730ec54aa2c5c3
Turtushig Gerelmaa <ffjzff@gmail.com>
2026-03-16 15:00:34 +0800

    feat(swipe): add customizable inbox swipe start and end actions with sanitization

--- app/src/main/kotlin/org/fossify/messages/helpers/Constants.kt
+++ app/src/main/kotlin/org/fossify/messages/helpers/Constants.kt
@@ -48,6 +48,14 @@ const val IS_ARCHIVE_AVAILABLE = "is_archive_available"
 const val CUSTOM_NOTIFICATIONS = "custom_notifications"
 const val IS_LAUNCHED_FROM_SHORTCUT = "is_launched_from_shortcut"
 const val KEEP_CONVERSATIONS_ARCHIVED = "keep_conversations_archived"
+const val INBOX_SWIPE_START_ACTION = "inbox_swipe_start_action"
+const val INBOX_SWIPE_END_ACTION = "inbox_swipe_end_action"
+
+const val INBOX_SWIPE_ACTION_NONE = 0
+const val INBOX_SWIPE_ACTION_ARCHIVE = 1
+const val INBOX_SWIPE_ACTION_TOGGLE_READ_STATUS = 2
+const val INBOX_SWIPE_ACTION_DELETE = 3
+const val INBOX_SWIPE_ACTION_BLOCK = 4
 
 private const val PATH = "org.fossify.org.fossify.messages.action."
 const val MARK_AS_READ = PATH + "mark_as_read"
